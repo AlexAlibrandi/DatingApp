ac87aa5d94743e8007d7fcf95154d7c9414ab2ff
Alex Alibrandi <alexander_alibrandi@hcl.com>
2021-09-28 15:26:42 -0400

    Created home, register pages linked register to service

---
+++ client/src/app/_services/account.service.ts
@@ -0,0 +1,53 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { ReplaySubject } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { User } from '../_models/user';
+
+//services are injectable
+//services are singleton it doesnt get destroyed until the application is down
+@Injectable({
+  providedIn: 'root',
+})
+//used to make requests to our api
+export class AccountService {
+  baseUrl = 'https://localhost:5001/api/';
+  private currentUserSource = new ReplaySubject<User>(1);
+  currentUser$ = this.currentUserSource.asObservable();
+
+  constructor(private http: HttpClient) {}
+
+  // receive our credentials from our login form from navbar
+  //populate our user in the local storgae of the browser
+  login(model: any) {
+    return this.http.post(this.baseUrl + 'account/login', model).pipe(
+      map((response: User) => {
+        const user = response;
+        if (user) {
+          localStorage.setItem('user', JSON.stringify(user));
+          this.currentUserSource.next(user);
+        }
+      })
+    );
+  }
+
+  register(model: any) {
+    return this.http.post(this.baseUrl + 'account/register', model).pipe(
+      map((user: User) => {
+        if (user) {
+          localStorage.setItem('user', JSON.stringify(user));
+          this.currentUserSource.next(user);
+        }
+      })
+    );
+  }
+
+  setCurrentUser(user: User) {
+    this.currentUserSource.next(user);
+  }
+
+  logout() {
+    localStorage.removeItem('user');
+    this.currentUserSource.next(null);
+  }
+}
--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,30 +1,29 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { User } from './_models/user';
+import { AccountService } from './_services/account.service';
 
 //decorator to create an angular component
 @Component({
-
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
   title = 'Union';
   users: any;
 
-  constructor(private http: HttpClient){}
+  constructor(private accountService: AccountService) {}
 
   //required method when appcomponent implements Oninit
-  ngOnInit(){
-    this.getUsers();
-    }
-
-    //creating method to get users
-    getUsers(){
-      this.http.get('https://localhost:5001/api/users').subscribe(response => {
-        this.users = response;
-      }, error => {
-        console.log(error)
-      })
-    }
+  ngOnInit() {
+    this.setCurrentUser();
   }
+
+  //getting the user object from our local storage
+  //setting that into our account service
+  setCurrentUser() {
+    const user: User = JSON.parse(localStorage.getItem('user'));
+    this.accountService.setCurrentUser(user)
+  }
+}
